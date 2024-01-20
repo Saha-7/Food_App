1c0fa26d6e53a2721287b89e10abe4432caf26ef
Pritam Saha <saha7pritam@gmail.com>
2024-01-20 20:15:41 +0530

    Add files via upload

---
+++ src/components/Grocery.js
@@ -0,0 +1,13 @@
+
+
+const Grocery = () => {
+    return(
+        <div>
+            <h1>
+                Welcome to our online Grocery store.
+            </h1>
+        </div>
+    )
+}
+
+export default Grocery
